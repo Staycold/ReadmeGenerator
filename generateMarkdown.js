a7070c0cbc85d3cb3be639ffb67203e89b6d37c7
patrick brodie <patrickxbrodie@hotmail.com>
2021-06-11 21:29:14 -0400

    index and module finished

---
+++ generateMarkdown.js
@@ -0,0 +1,90 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+
+function renderLicenseBadge(license) {
+    switch (license) {
+      case "PERL":
+        return `[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)]`
+      case "MIT":
+        return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`
+      case "SIL":
+        return ` [![License: Open Font-1.1](https://img.shields.io/badge/License-OFL%201.1-lightgreen.svg)]`
+      default:
+        return "";
+  
+    }
+  }
+  
+    // TODO: Create a function that returns the license link
+    // If there is no license, return an empty string
+    function renderLicenseLink(license) {
+      switch (license) {
+        case "PERL":
+          return `(https://opensource.org/licenses/Artistic-2.0)`
+        case "MIT":
+          return `(https://opensource.org/licenses/MIT)`
+        case "SIL":
+          return `(https://opensource.org/licenses/OFL-1.1)`
+        default:
+          return "";
+    
+      }
+    }
+  
+    // TODO: Create a function that returns the license section of README
+    // If there is no license, return an empty string
+    function renderLicenseSection(license) {
+      // use if or switch
+      switch (license) {
+        case "PERL":
+          return `[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)]`
+        case "MIT":
+          return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`
+        case "SIL":
+          return ` [![License: Open Font-1.1](https://img.shields.io/badge/License-OFL%201.1-lightgreen.svg)]`
+        default:
+          return "";
+    
+      }
+    }
+  
+    // TODO: Create a function to generate markdown for README
+    function generateMarkdown(response) {
+  
+      
+      return ` ${renderLicenseBadge(response.license)}
+  
+      ##${response.tile}
+  
+  
+      ## Table of contents
+    \n*[Contributing](#contributing)
+    \n*[Description](#description)
+    \n*[Installation](#installation)
+    \n*[Usage](#usage)
+    \n*[License](#license)
+    \n*[Tests](#tests)
+    \n*[Questions](#questions)
+    
+  ## Description
+  ${response.desc}
+  ## Installation
+  ${response.install}
+  ## Usage
+  ${response.usage}
+  ## License
+  
+  ${renderLicenseLink(response.license)}
+  ## Contributing
+  ${response.contribute}
+  ## Tests
+  ${response.tests}
+  ## Questions
+  If there are any questions, please email me at: ${response.email},
+  or view my GitHub for more work: https://github.com/${response.gitHub}
+  `
+  
+        ;
+    }
+  
+    module.exports = generateMarkdown;
---
+++ index.js
@@ -0,0 +1,83 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require("fs");
+const generateMarkdown = require('./utils/generateMarkdown.js')
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+      type: 'input',
+      message: 'What is the title of your project?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Please drscribe your project',
+      name: 'desc',
+    },
+    
+    {
+      type: 'input',
+      message: 'How does one install your project?',
+      name: 'install',
+    },
+    {
+      type: 'input',
+      message: 'please provide usage information',
+      name: 'usage',
+    },
+    {
+      type: 'list',
+      message: 'please select a license',
+      name:'license',
+      choices: ['MIT', 'PERL', 'SIL']
+    },
+    {
+      type: 'input',
+      message: 'please provide details on how to contribute',
+      name: 'contribute',
+    },
+    {
+      type: 'input',
+      message: 'What tests should be run?',
+      name: 'tests',
+    },
+    {
+      type: 'input',
+      message: 'Please provide your email',
+      name: 'email',
+    },
+    {
+      type: 'input',
+      message: 'Please provide your GitHub username',
+      name: 'gitHub',
+    }
+
+];
+
+
+// TODO: Create a function to write README file
+
+
+function writeToFile(fileName, data) {
+  fs.writeFile(`${fileName}`,data,err=>{
+err?console.log(err):console.log('success')
+  }
+  )}
+
+// TODO: Create a function to initialize app
+function init() {
+
+
+  inquirer.prompt(questions)
+.then((response) => {
+  console.log(response)
+const result= generateMarkdown(response)
+writeToFile(`${response.title}.md`,result)
+})
+}
+
+// Function call to initialize app
+init();
+
+
+// , , Usage, License, Contributing, Tests, and Questions
